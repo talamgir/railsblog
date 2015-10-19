ac19f8a8d2225eb8f0d4f2f26ba6e921ca362dfc
Taimur Alamgir <talamgir@law.gwu.edu>
2015-10-19 15:01:40 -0400

    pre-devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,22 +6,20 @@ class User < ActiveRecord::Base
 	has_one :profile, through: :user_profile
 
 
-	attr_accessor :password
-
-	before_save :encrypt_password
+	# attr_accessor :password
 
 
-	def encrypt_password
-		self.password_salt = BCrypt::Engine.generate_salt
-		self.password_hash = BCrypt::Engine.hash_secret(password, password_salt)
-	end
+	# # def encrypt_password
+	# # 	self.password_salt = BCrypt::Engine.generate_salt
+	# # 	self.password_hash = BCrypt::Engine.hash_secret(password, password_salt)
+	# # end
 
-	def self.authenticate(email, password)
-		user = User.where(email: email).first
-		if user && user.password_hash == BCrypt::Engine.hash_secret(password, user.password_salt)
-				user
-			else
-				nil
-			end
-		end
-	end
+	# def self.authenticate(email, password)
+	# 	user = User.where(email: email).first
+	# 	if user && user.password_hash == BCrypt::Engine.hash_secret(password, user.password_salt)
+	# 			user
+	# 		else
+	# 			nil
+	# 		end
+	# 	end
+end
